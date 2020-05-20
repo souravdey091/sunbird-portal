b275f6699d6f68ec2d4e90181d69556814e9685d
Devesh <mailme.devmehta@gmail.com>
2020-05-20 17:13:00 +0530

    Issue #SH-68 switch and create profile of managed users

--- src/app/client/src/app/plugins/profile/components/choose-user/choose-user.component.ts
+++ src/app/client/src/app/plugins/profile/components/choose-user/choose-user.component.ts
@@ -1,7 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import {ProfileService} from './../../services';
 import {UserService} from "@sunbird/core";
-import {ServerResponse, ToasterService} from "@sunbird/shared";
+import {ConfigService, ServerResponse, ToasterService} from "@sunbird/shared";
+import {Router} from '@angular/router';
+import {TelemetryService} from "@sunbird/telemetry";
+import {environment} from "@sunbird/environment";
+import * as _ from 'lodash-es';
 
 @Component({
   selector: 'app-choose-user',
@@ -11,7 +15,8 @@ import {ServerResponse, ToasterService} from "@sunbird/shared";
 export class ChooseUserComponent implements OnInit {
 
   constructor(public profileService: ProfileService, public userService: UserService,
-              public toasterService: ToasterService) {
+              public toasterService: ToasterService, public router: Router,
+              private telemetryService: TelemetryService, private configService: ConfigService) {
   }
 
   ngOnInit() {
@@ -22,7 +27,7 @@ export class ChooseUserComponent implements OnInit {
     const fetchManagedUserRequest = {
       request: {
         filters: {
-          managedBy: this.userService.userid
+          managedBy: 'de35f92e-08a9-462d-b9e0-4d909c37227c' || this.userService.userid
         },
         offset: 0,
         limit: 20
@@ -39,11 +44,49 @@ export class ChooseUserComponent implements OnInit {
 
   switchUser(userId) {
     this.profileService.initiateSwitchUser(userId).subscribe((data) => {
-        console.log(data);
-
+      console.log(data);
+      // @ts-ignore
+      document.getElementById('userId').value = 'de35f92e-08a9-462d-b9e0-4d909c37227c';
+      this.userService.setUserId('de35f92e-08a9-462d-b9e0-4d909c37227c');
+      this.userService.initialize(true);
+      this.telemetryService.initialize(this.getTelemetryContext());
       }, (err) => {
         // this.toasterService.error(_.get(this.resourceService, 'messages.fmsg.m0004'));
       }
     );
+  }
+
+  navigateToCreateUser() {
+    this.router.navigate(['/profile/create-user']);
+  }
+
+  getTelemetryContext() {
+    const userProfile = this.userService.userProfile;
+    const buildNumber = (<HTMLInputElement>document.getElementById('buildNumber'));
+    const version = buildNumber && buildNumber.value ? buildNumber.value.slice(0, buildNumber.value.lastIndexOf('.')) : '1.0';
+    return {
+      userOrgDetails: {
+        userId: userProfile.userId,
+        rootOrgId: userProfile.rootOrgId,
+        rootOrg: userProfile.rootOrg,
+        organisationIds: userProfile.hashTagIds
+      },
+      config: {
+        pdata: {
+          id: this.userService.appId,
+          ver: version,
+          pid: this.configService.appConfig.TELEMETRY.PID
+        },
+        endpoint: this.configService.urlConFig.URLS.TELEMETRY.SYNC,
+        apislug: this.configService.urlConFig.URLS.CONTENT_PREFIX,
+        host: '',
+        uid: userProfile.userId,
+        sid: this.userService.sessionId,
+        channel: _.get(userProfile, 'rootOrg.hashTagId'),
+        env: 'home',
+        enableValidation: environment.enableTelemetryValidation,
+        timeDiff: this.userService.getServerTimeDiff
+      }
+    }
   }
 }
