a2a35a074eda7e4d837394fd120d00043d866b22
Sourav <sourav_d@techjoomla.com>
2020-05-21 16:15:54 +0530

    Issue #SH-139 feat: User create

--- src/app/client/src/app/plugins/profile/components/create-user/create-user.component.ts
+++ src/app/client/src/app/plugins/profile/components/create-user/create-user.component.ts
@@ -1,12 +1,12 @@
-import {Component, OnInit, OnDestroy} from '@angular/core';
-import {ResourceService, ToasterService, ServerResponse, UtilService} from '@sunbird/shared';
-import {ProfileService} from './../../services';
-import {FormBuilder, Validators, FormGroup, FormControl} from '@angular/forms';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ResourceService, ToasterService, ServerResponse, UtilService } from '@sunbird/shared';
+import { ProfileService } from './../../services';
+import { FormBuilder, Validators, FormGroup, FormControl } from '@angular/forms';
 import * as _ from 'lodash-es';
-import {IInteractEventObject, IInteractEventEdata} from '@sunbird/telemetry';
-import {OrgDetailsService, ChannelService, FrameworkService, UserService} from '@sunbird/core';
-import {Subscription, Subject} from 'rxjs';
-import {Router} from '@angular/router';
+import { IInteractEventObject, IInteractEventEdata } from '@sunbird/telemetry';
+import { OrgDetailsService, ChannelService, FrameworkService, UserService, FormService } from '@sunbird/core';
+import { Subscription, Subject } from 'rxjs';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -24,36 +24,54 @@ export class CreateUserComponent implements OnInit, OnDestroy {
   termsAndConditionLink: any;
   showTncPopup = false;
   instance: string;
+  formData;
+  showLoader = true;
 
   constructor(public resourceService: ResourceService, public toasterService: ToasterService,
-              public profileService: ProfileService, formBuilder: FormBuilder, public router: Router,
-              public userService: UserService, public orgDetailsService: OrgDetailsService, public channelService: ChannelService,
-              public frameworkService: FrameworkService, public utilService: UtilService) {
+    public profileService: ProfileService, formBuilder: FormBuilder, public router: Router,
+    public userService: UserService, public orgDetailsService: OrgDetailsService, public channelService: ChannelService,
+    public frameworkService: FrameworkService, public utilService: UtilService, public formService: FormService) {
     this.sbFormBuilder = formBuilder;
   }
 
   ngOnInit() {
     this.instance = _.upperCase(this.resourceService.instance || 'SUNBIRD');
     this.fetchTncData();
-    this.initializeFormFields();
+    this.getFormDetails();
     this.userProfile = this.userService.userProfile;
+  }
+
+  getFormDetails() {
+    const formServiceInputParams = {
+      formType: 'user',
+      formAction: 'create',
+      contentType: 'child',
+      component: 'portal'
+    };
+    this.formService.getFormConfig(formServiceInputParams, this.userService.hashTagId).subscribe((formData) => {
+      this.formData = formData;
+      this.initializeFormFields();
+    }, (err) => {
+      this.toasterService.error(_.get(this.resourceService, 'messages.emsg.m0005'));
+      this.showLoader = false;
+    });
   }
 
   fetchTncData() {
     this.profileService.getTncConfig().subscribe((data: ServerResponse) => {
-        const response = _.get(data, 'result.response.value');
-        if (response) {
-          try {
-            const tncConfig = this.utilService.parseJson(response);
-            this.tncLatestVersion = _.get(tncConfig, 'latestVersion') || {};
-            this.termsAndConditionLink = tncConfig[this.tncLatestVersion].url;
-          } catch (e) {
-            this.toasterService.error(_.get(this.resourceService, 'messages.fmsg.m0004'));
-          }
+      const response = _.get(data, 'result.response.value');
+      if (response) {
+        try {
+          const tncConfig = this.utilService.parseJson(response);
+          this.tncLatestVersion = _.get(tncConfig, 'latestVersion') || {};
+          this.termsAndConditionLink = tncConfig[this.tncLatestVersion].url;
+        } catch (e) {
+          this.toasterService.error(_.get(this.resourceService, 'messages.fmsg.m0004'));
         }
-      }, (err) => {
-        this.toasterService.error(_.get(this.resourceService, 'messages.fmsg.m0004'));
       }
+    }, (err) => {
+      this.toasterService.error(_.get(this.resourceService, 'messages.fmsg.m0004'));
+    }
     );
   }
 
@@ -62,18 +80,25 @@ export class CreateUserComponent implements OnInit, OnDestroy {
   }
 
   initializeFormFields() {
-    this.userDetailsForm = this.sbFormBuilder.group({
-      name: new FormControl(null, [Validators.required]),
-      checkbox: new FormControl(null, [Validators.required])
-    }, {
+    const formGroupObj = {};
+    for (const key of this.formData) {
+      if (key.visible && key.required) {
+        formGroupObj[key.code] = new FormControl(null, [Validators.required]);
+      } else if (key.visible) {
+        formGroupObj[key.code] = new FormControl(null);
+      }
+    }
+
+    this.userDetailsForm = this.sbFormBuilder.group(formGroupObj, {
       validator: (formControl) => {
         const nameCtrl = formControl.controls.name;
         if (_.trim(nameCtrl.value) === '') {
-          nameCtrl.setErrors({required: true});
+          nameCtrl.setErrors({ required: true });
         }
         return null;
       }
     });
+    this.showLoader = false;
     this.enableSubmitBtn = (this.userDetailsForm.status === 'VALID');
     this.enableSubmitButton();
   }
@@ -95,18 +120,18 @@ export class CreateUserComponent implements OnInit, OnDestroy {
       }
     };
     this.userService.registerUser(createUserRequest).subscribe((resp: ServerResponse) => {
-        const requestBody = {
-          request: {
-            version: this.userProfile.tncLatestVersion,
-            userId: resp.result.userId
-          }
-        };
-        this.userService.acceptTermsAndConditions(requestBody).subscribe(res => {
-          this.router.navigate(['/profile/choose-user']);
-        }, err => {
-          this.toasterService.error(this.resourceService.messages.fmsg.m0085);
-        });
-      },
+      const requestBody = {
+        request: {
+          version: this.userProfile.tncLatestVersion,
+          userId: resp.result.userId
+        }
+      };
+      this.userService.acceptTermsAndConditions(requestBody).subscribe(res => {
+        this.router.navigate(['/profile/choose-user']);
+      }, err => {
+        this.toasterService.error(this.resourceService.messages.fmsg.m0085);
+      });
+    },
       (err) => {
         this.toasterService.error(this.resourceService.messages.fmsg.m0085);
       }
